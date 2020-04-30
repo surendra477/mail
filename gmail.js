f9860b32dbbd2e32429e744393152832e08cf1c3
surendra477 <58383796+surendra477@users.noreply.github.com>
2020-04-30 19:06:20 +0530

    Update gmail.js

--- gmail.js
+++ gmail.js
@@ -1,17 +1,19 @@
-
+//first of all to run this u should allow your mail to less secure app access in google
+//link of it is https://myaccount.google.com/lesssecureapps
+//you should install npm install nodemailer
 var nodemailer = require('nodemailer');
 
 var transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-        user: 'yourmail@gmail.com',
-        pass: '123456789'
+        user: 'yourmail@gmail.com',//your mail
+        pass: '123456789'   //your password
     }
 });
 
 var mailOptions = {
-    from: 'yourmail@gmail.com',
-    to: 'friendmail@gmail.com',
+    from: 'yourmail@gmail.com',//your mail
+    to: 'friendmail@gmail.com',//your password u can send to multiple by using , and another mail
     subject: 'Sending Email using Node.js',
     text: 'That was easy!'
 };
